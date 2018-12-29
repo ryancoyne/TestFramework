0c9dbee0e506aa7bc1c07321e0edbcc1dcbc7537
Ryan Coyne <ryan@clearcodex.com>
2018-12-28 21:00:41 -0500

    Adding in the test file

---
+++ TestFramework/Test.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  TestFramework
+//
+//  Created by Ryan Coyne on 12/28/18.
+//  Copyright © 2018 ClearCodeX Inc. All rights reserved.
+//
+
+import Foundation
+
+struct Test {
+    private init() { }
+    
+    func test() {
+        print("Yay.  You tested successfully!")
+    }
+}
